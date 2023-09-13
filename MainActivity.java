5dd937614b544c3e5499ee0b0c33401a12d0c9d9
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-09-13 11:37:18 +0100

    Update MainActivity.java
    Added a basic skeleton of what the first Android screen will look like.

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,45 @@
+package com.example.smishingdetector; //This is the name of the package
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import cloud.pangeacyber.pangea.*; //We use this import declaration to obtain the various APIs provided by Pangea.
 
+public class MainActivity extends AppCompatActivity { //This begins the MainActivity screen.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //This begins the onCreate() stage of the Android app lifecycle. 
+        super.onCreate(savedInstanceState);
+      
+    }
+
+    @Override
+    protected void onResume(){ //This begins the onResume() stage of the Android app lifecycle.
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart(){ //This begins the onStart() stage of the Android app lifecycle.
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop(){ //This begins the onStop() stage of the Android app lifecycle.
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause(){ //This begins the onPause() stage of the Android app lifecycle.
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart(){ //This begins the onRestart() stage of the Android app lifecycle.
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy(){ //This begins the onDestroy() stage of the Android app lifecycle.
+        super.onDestroy();
+    }
+
+
+}
